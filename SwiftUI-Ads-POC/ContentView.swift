be5b381b1f147ab1bae5f5a68c439fd1ca14e25f
Anirban Deb <anirbandebtanu.uts@gmail.com>
2025-06-11 02:51:20 +0600

    Initial Commit

---
+++ SwiftUI-Ads-POC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-Ads-POC
+//
+//  Created by Anirban Deb on 11/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-Ads-POC/SwiftUI_Ads_POCApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Ads_POCApp.swift
+//  SwiftUI-Ads-POC
+//
+//  Created by Anirban Deb on 11/6/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Ads_POCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
