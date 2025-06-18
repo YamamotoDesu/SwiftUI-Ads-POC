c24939c8ec67a7837625035955358a0e4ffab608
Anirban Deb <anirbandebtanu.uts@gmail.com>
2025-06-19 00:06:31 +0600

    Update README.md

---
+++ SwiftUI-Ads-POC/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUI-Ads-POC
+//
+//  Created by Anirban Deb on 11/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Your App Content")
+                .padding()
+            
+            Spacer()
+            
+            BannerAdView()
+                .frame(height: 50)
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
